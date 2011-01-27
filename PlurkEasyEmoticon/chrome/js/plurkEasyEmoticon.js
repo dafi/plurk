0623b64efce1d9daed0da6097a26b181ae3a18d8
Davide Ficano <davide.ficano@gmail.com>
2011-01-27 19:02:45 +0100

    Version 1.3.1 When div height is grater that barHeight the div hides while mouse pointer is on emoticons

--- PlurkEasyEmoticon/chrome/js/plurkEasyEmoticon.js
+++ PlurkEasyEmoticon/chrome/js/plurkEasyEmoticon.js
@@ -1,136 +1,25 @@
 var plurkEasyEmoticon = {
-    basicSet:[
-        {imgUrl:'http://statics.plurk.com/ff124032f8cc3a9d43b99e661f8fcb4d.gif', codeText:':-))'},
-        {imgUrl:'http://statics.plurk.com/99ef3957ef779718546752b749bdeabd.gif', codeText:':-)'},
-        {imgUrl:'http://statics.plurk.com/3385896779bf1c13188bf92ca516878e.gif', codeText:':-D'},
-        {imgUrl:'http://statics.plurk.com/615f18f7ea8abc608c4c20eaa667883b.gif', codeText:'(LOL)'},
-        {imgUrl:'http://statics.plurk.com/2d5e21929e752498e36d74096b1965e1.gif', codeText:':-P'},
-        {imgUrl:'http://statics.plurk.com/13b15aa49358be8f47b58552401d7725.gif', codeText:'(woot)'},
-        {imgUrl:'http://statics.plurk.com/57c69f50e40a283dcd2e7b56fc191abe.gif', codeText:';-)'},
-        {imgUrl:'http://statics.plurk.com/8eb05ca7a32301ba16c9496bcad893c4.gif', codeText:':-o'},
-        {imgUrl:'http://statics.plurk.com/261c0fe4a88417146ae0292d697a5f52.gif', codeText:'X-('},
-        {imgUrl:'http://statics.plurk.com/11eed61b41a3e935773476ac33bc00d9.gif', codeText:':-('},
-        {imgUrl:'http://statics.plurk.com/72ddf2c585fe77dd0be731b19624d8cb.gif', codeText:':\'-('},
-        {imgUrl:'http://statics.plurk.com/2884b8d0e496c06136c86e9c9599edae.gif', codeText:':-&'},
-        {imgUrl:'http://statics.plurk.com/9454d15bcaf411b159dcc147ebc3f0eb.gif', codeText:'(K)'},
-        {imgUrl:'http://statics.plurk.com/a5ae31c4185bc60cd006650dc10f8147.gif', codeText:'(angry)'},
-        {imgUrl:'http://statics.plurk.com/35b16fc25623670e41c2be6bf8ac38c7.gif', codeText:'(annoyed)'},
-        {imgUrl:'http://statics.plurk.com/4afd784c0df9f7a3ceacb92beca543f6.gif', codeText:'(wave)'},
-        {imgUrl:'http://statics.plurk.com/c1c9870cf653fa3cd103d2eb0f519ccb.gif', codeText:'B-)'},
-        {imgUrl:'http://statics.plurk.com/d1a6f08507b126ec6a215e6a2372e8bb.gif', codeText:'(cozy)'},
-        {imgUrl:'http://statics.plurk.com/5495d64ccb898ca596b061168fa0374a.gif', codeText:'(sick)'},
-        {imgUrl:'http://statics.plurk.com/b82e3225c92a764d225429a6487d9f04.gif', codeText:'(:'},
-        {imgUrl:'http://statics.plurk.com/65271ac2126706bc09d31ff67c525d67.gif', codeText:'(goodluck)'},
-        {imgUrl:'http://statics.plurk.com/a709dab8ddd26bd222466d31bd549579.png', codeText:'(griltongue)'},
-        {imgUrl:'http://statics.plurk.com/e3baa9d0d78c35e955a6b07c39f530fa.gif', codeText:'(mmm)'},
-        {imgUrl:'http://statics.plurk.com/0f96595ed7733393b93a3d67aa4f2f4f.gif', codeText:'(hungry)'},
-        {imgUrl:'http://statics.plurk.com/919b87048fdf7bd59dae457f4284b20b.gif', codeText:'(music)'},
-        {imgUrl:'http://statics.plurk.com/96872d481bbfe87aad5aed976c7de4ee.gif', codeText:'(tears)'},
-        {imgUrl:'http://statics.plurk.com/56336bb821c4766001816639e55e5811.gif', codeText:'(tongue)'},
-        {imgUrl:'http://statics.plurk.com/6cb1dc388b9259565efedef8f336d27d.gif', codeText:'(unsure)'},
-        {imgUrl:'http://statics.plurk.com/a9560787e93f4f8890e4bd38696ba537.gif', codeText:'(highfive)'},
-        {imgUrl:'http://statics.plurk.com/a55bdb344892676b0fea545354654a49.gif', codeText:'(dance)'},
-        {imgUrl:'http://statics.plurk.com/9939dd585cf0e8d39e7912a98a9ce727.gif', codeText:'(blush)'}
-    ],
-
-    extraSet:[
-        {imgUrl:'http://statics.plurk.com/e8ed6c7eed76d2acd9dbf469f29fbec2.gif', codeText:'(doh)'},
-        {imgUrl:'http://statics.plurk.com/2b3593aea68efa7a00b4ef2850f98b8a.gif', codeText:'(brokenheart)'},
-        {imgUrl:'http://statics.plurk.com/ed3620ff28a02e3dc9ac4ffa8e6ae2e6.gif', codeText:'(drinking)'},
-        {imgUrl:'http://statics.plurk.com/08a43d50691a1ed22706fc92f568fa07.gif', codeText:'(girlkiss)'},
-        {imgUrl:'http://statics.plurk.com/8600839dc03e6275b53fd03a0eba09cf.gif', codeText:'(rofl)'},
-        {imgUrl:'http://statics.plurk.com/e6bb16b6ef386c5f23900b103dbdba31.gif', codeText:'(money)'},
-        {imgUrl:'http://statics.plurk.com/1c890273544559b17f090d09238fa763.gif', codeText:'(rock)'},
-        {imgUrl:'http://statics.plurk.com/f053074bcce500fbd1e2327d49748a6d.gif', codeText:'(nottalking)'},
-        {imgUrl:'http://statics.plurk.com/1f44d3984a094fceae1f1a016a730fc9.gif', codeText:'(party)'},
-        {imgUrl:'http://statics.plurk.com/2f7c90ce56fb4a70e34c04d8d7692dd0.gif', codeText:'(sleeping)'},
-        {imgUrl:'http://statics.plurk.com/900f3dd0adaad9142d567caf6bfb1311.gif', codeText:'(thinking)'},
-        {imgUrl:'http://statics.plurk.com/95ace5ba1097301b5206a9e0fb431624.gif', codeText:'(bringit)'},
-        {imgUrl:'http://statics.plurk.com/95e69aa508d4bb435706b9db0a610dad.gif', codeText:'(worship)'},
-        {imgUrl:'http://statics.plurk.com/a08ed27ec14b48d4703f53f7eb94834b.gif', codeText:'(applause)'},
-        {imgUrl:'http://statics.plurk.com/85ef5fa01a6a67a525429f8bf4279fe7.gif', codeText:'8-)'},
-        {imgUrl:'http://statics.plurk.com/986ecf2b1ae69072e0195b0a58545900.gif', codeText:'(gym)'},
-        {imgUrl:'http://statics.plurk.com/150e3f367a063d3baf9720719d78d778.gif', codeText:'(heart)'},
-        {imgUrl:'http://statics.plurk.com/3fabe74e992888be701de2a9d80c180a.gif', codeText:'(devil)'},
-        {imgUrl:'http://statics.plurk.com/92b595a573d25dd5e39a57b5d56d4d03.gif', codeText:'(lmao)'},
-        {imgUrl:'http://statics.plurk.com/4f01bac8a707e5450307f97065ec0fa7.gif', codeText:'(banana_cool)'},
-        {imgUrl:'http://statics.plurk.com/48515125401120316abb97666458d05b.gif', codeText:'(banana_rock)'},
-        {imgUrl:'http://statics.plurk.com/aabbc82c2b0dc72bfbce2f82c97a95e8.gif', codeText:'(evil_grin)'},
-        {imgUrl:'http://statics.plurk.com/b0b0596acce9ffc1f2a27548aa642eaf.gif', codeText:'(headspin)'},
-        {imgUrl:'http://statics.plurk.com/52991d7ff65a05526454bd1170a0f14c.gif', codeText:'(heart_beat)'},
-        {imgUrl:'http://statics.plurk.com/846277f0a154dc95a08594b7d32a5ccd.gif', codeText:'(ninja)'},
-        {imgUrl:'http://statics.plurk.com/843739a95294fd0bf4c958840b46408f.gif', codeText:'(haha)'},
-        {imgUrl:'http://statics.plurk.com/22416dced8b59446db8cd366cc925d09.gif', codeText:'(evilsmirk)'},
-        {imgUrl:'http://statics.plurk.com/e3f0f67ca3af62e34f13abf1d036a010.gif', codeText:'(eyeroll)'}
-    ],
-
-    karma100: [
-        {imgUrl:'http://statics.plurk.com/84f94a47fcaf1df0a5f17a1cfa52fa64.gif', codeText:'(muhaha)'},
-        {imgUrl:'http://statics.plurk.com/44117848701cd748460921cfea5c3781.gif', codeText:'(taser)'},
-        {imgUrl:'http://statics.plurk.com/88f6dda8d290f66a923c1116a2a2b4ab.gif', codeText:'(banana_ninja)'},
-        {imgUrl:'http://statics.plurk.com/eeaf87c619a0221ec9fa06413fd2d5dc.gif', codeText:'(beer)'},
-        {imgUrl:'http://statics.plurk.com/48ec47723cb34be3fcbc914e591e69cd.gif', codeText:'(coffee)'},
-        {imgUrl:'http://statics.plurk.com/259a728a690204148037fbee7e2ca2d3.gif', codeText:'(fish_hit)'},
-        {imgUrl:'http://statics.plurk.com/4383af0e055bce112176c5104deeaadf.gif', codeText:'(muscle)'},
-        {imgUrl:'http://statics.plurk.com/70722ab5756c3b89c86d85feab91725d.gif', codeText:'(smileydance)'},
-        {imgUrl:'http://statics.plurk.com/91cf07e3aa16738943fa1147940b48ea.gif', codeText:'(morning)'}
-    ],
-
-    hiddenSet:[
-        {imgUrl:'http://statics.plurk.com/7256dae81d56d150120ccd0c96dd2197.gif', codeText:'(fireworks)'},
-        {imgUrl:'http://statics.plurk.com/47d20905d017c396d67b4a30c9ac9b10.png', codeText:'(goal)'},
-        {imgUrl:'http://statics.plurk.com/4ad099fba019942f13058610ff3fc568.gif', codeText:'(dance_bzz)'},
-        {imgUrl:'http://statics.plurk.com/5a2a63fa773e68797ec69a1303bfa3b9.png', codeText:'(bzzz)'},
-        {imgUrl:'http://statics.plurk.com/deda4d9f78ad528d725e3a6bfbf6352f.gif', codeText:'(\u0420\u0443\u0441\u0441\u043A\u0438\u0439)'},
-        {imgUrl:'http://statics.plurk.com/0efc4d55d28704f4370ef874ae906161.gif', codeText:'(code)'},
-        {imgUrl:'http://statics.plurk.com/4c40d16a0d369b895c08f2e33d062ec8.gif', codeText:'(yarr)'},
-        {imgUrl:'http://statics.plurk.com/6675254cd7449b1847a93b0024127eae.gif', codeText:'(angry_okok)'},
-        {imgUrl:'http://statics.plurk.com/8855f56400a936db07f348d9290adaac.gif', codeText:'(code_okok)'},
-        {imgUrl:'http://statics.plurk.com/fcd28d7d78ec1f828c76930fa63270e6.gif', codeText:'(gym_okok)'},
-        {imgUrl:'http://statics.plurk.com/71acd802cc931649dd9a371ccf70bad2.gif', codeText:'(hungry_okok)'},
-        {imgUrl:'http://statics.plurk.com/74030f05f06547a3d26b02ccbf0bbac7.gif', codeText:'(music_okok)'},
-        {imgUrl:'http://statics.plurk.com/3acbaf42504fff32c5eac4f12083ce56.gif', codeText:'(yarr_okok)'},
-        {imgUrl:'http://statics.plurk.com/bac8c8392f7ca8f5ac74612be4d08b74.gif', codeText:'(wave_okok)'},
-        {imgUrl:'http://statics.plurk.com/feb43dbbbf2763905571060be9a496d1.gif', codeText:'(no_dance)'},
-        {imgUrl:'http://statics.plurk.com/5b51892d7d1f392d93ea7fe26e5100f4.gif', codeText:'(banana_gym)'},
-        {imgUrl:'http://statics.plurk.com/a555399b40c379adca5b6f5bad5bf732.gif', codeText:'(dance_okok)'},
-        {imgUrl:'http://statics.plurk.com/6de58c967f1c2797d250a713ba50eddd.gif', codeText:'(dance_yarr)'},
-        {imgUrl:'http://statics.plurk.com/88fac5a4b99110a35d4e4794dad58ab4.gif', codeText:'(taser_okok)'},
-        {imgUrl:'http://statics.plurk.com/b3b9856e557fcc2700fd41c53f9d4910.gif', codeText:'(droid_dance)'},
-        //{imgUrl:'http://statics.plurk.com/1a5f23ed863e70e52f239b045a48e6fb.gif', codeText:'(xmas1)'},
-        //{imgUrl:'http://statics.plurk.com/f5dbd5fdf5f5df69cfb024d6be76a76b.gif', codeText:'(xmas2)'},
-        //{imgUrl:'http://statics.plurk.com/e902170e97aee14836b5df6b0fe61ba2.gif', codeText:'(xmas3)'},
-        //{imgUrl:'http://statics.plurk.com/e476574723d5042f24658fa36866bd92.gif', codeText:'(xmas4)'},
-        {imgUrl:'http://statics.plurk.com/cfdd2accc1188f5fbc62e149074c7f29.png', codeText:'(fuu)'},
-        {imgUrl:'http://statics.plurk.com/828b9819249db696701ae0987fba3638.png', codeText:'(gfuu)'},
-        {imgUrl:'http://statics.plurk.com/1bd653e166492e40e214ef6ce4dd716f.png', codeText:'(yay)'},
-        {imgUrl:'http://statics.plurk.com/3fe6cf919158597d7ec74f8d90f0cc9f.png', codeText:'(gyay)'},
-        {imgUrl:'http://statics.plurk.com/9c5c54081547d2ad903648f178fcc595.png', codeText:'(bah)'},
-        {imgUrl:'http://statics.plurk.com/2da76999ca3716fb4053f3332270e5c9.png', codeText:'(gbah)'},
-        {imgUrl:'http://statics.plurk.com/f73b773aa689647cb09f57f67a83bb89.png', codeText:'(troll)'},
-        {imgUrl:'http://statics.plurk.com/45beda260eddc28c82c0d27377e7bf42.png', codeText:'(gtroll)'},
-
-        {imgUrl:'http://statics.plurk.com/8590888362ae83daed52e4ca73c296a6.png', codeText: '(aha)'},
-        {imgUrl:'http://statics.plurk.com/c7551098438cc28ec3b54281d4b09cc3.png', codeText: '(gaha)'},
-        {imgUrl:'http://statics.plurk.com/cfd84315ebceec0c4389c51cf69132bd.png', codeText: '(whatever)'},
-        {imgUrl:'http://statics.plurk.com/0e0bf1ec2c2958799666f3995ef830ca.png', codeText: '(gwhatever)'},
-        {imgUrl:'http://statics.plurk.com/e2998ca75f80c1c4a5508c549e3980a6.png', codeText: '(pokerface)'},
-        {imgUrl:'http://statics.plurk.com/c6ad1c4f9e11f6859a1ba39c4341ef8b.png', codeText: '(gpokerface)'},
-        {imgUrl:'http://statics.plurk.com/4a61085f1c6a639f028cd48ae97d07d0.png', codeText: '(yea)'},
-        {imgUrl:'http://statics.plurk.com/53253ca60f5831f0812954213a2e9bb3.png', codeText: '(gyea)'}
-        ],
-
     easyDiv: document.getElementById('plurkEasyEmoticonDiv'),
     isBarVisible: false,
     barHeight: 96,
     lastInputFocused: null,
     isEnabled: true,
+    allEmoticons: null,
 
     init: function() {
+        var result = PlurkAPI.getEmoticons();
+        if (result.status != 200) {
+            return;
+        }
+
         if (typeof(window.localStorage['enabled']) == 'undefined') {
             window.localStorage['enabled'] = 'true';
         }
         this.isEnabled = window.localStorage['enabled'] == 'true'
+
+        this.allEmoticons = result.data;
+        var karmaEmots = this.allEmoticons['karma'];
+        var recruitedEmots = this.allEmoticons['recuited'];
 
         this.updater = document.getElementById('updater');
         this.updaterZIndex = this.updater ? this.updater.style.zIndex : '';
@@ -138,7 +27,7 @@ var plurkEasyEmoticon = {
         this.easyDiv = document.createElement('div');
         this.easyDiv.setAttribute('id', 'plurkEasyEmoticonDiv');
         this.addClass(this.easyDiv, 'bottom-bar');
-        var allSets = [this.basicSet, this.extraSet];
+        var allSets = [];
 
         // valid only from bookmarklet
         //var karma = typeof(SiteState) != 'undefined'
@@ -156,17 +45,22 @@ var plurkEasyEmoticon = {
             && parseFloat(karma.firstChild.nodeValue)
             || 100;
 
-        if (karma >= 100) {
-            allSets.push(this.karma100);
+        for (var i in karmaEmots) {
+            if (karma >= parseFloat(i)) {
+                allSets.push(karmaEmots[i]);
+            }
         }
-        allSets.push(this.hiddenSet);
+        // TODO: add check using API to determine if user can use recruitedEmots
+        for (var i in recruitedEmots) {
+            allSets.push(recruitedEmots[i]);
+        }
         for (var s in allSets) {
             var currSet = allSets[s];
             for (var i in currSet) {
                 var eicon = currSet[i];
                 var img = document.createElement('img');
-                img.setAttribute('alt', eicon.codeText);
-                img.setAttribute('src', eicon.imgUrl);
+                img.setAttribute('alt', eicon[0]);
+                img.setAttribute('src', eicon[1]);
                 img.addEventListener('click', function(event) {
                     if (!plurkEasyEmoticon.lastInputFocused) {
                         return;
@@ -206,28 +100,33 @@ var plurkEasyEmoticon = {
         this.isBarVisible = true;
         window.addEventListener('mousemove', function(event) {
             var y = document.documentElement.clientHeight - event.clientY - 16;
-            
-            if (plurkEasyEmoticon.isBarVisible) {
-                if (y > plurkEasyEmoticon.barHeight) {
-                    plurkEasyEmoticon.easyDiv.style.display = 'none';
+            if (plurkEasyEmoticon.isEnabled) {
+                if (y < plurkEasyEmoticon.barHeight) {
+                    plurkEasyEmoticon.easyDiv.style.display = 'block';
                     if (plurkEasyEmoticon.updater) {
-                        plurkEasyEmoticon.updater.style.zIndex = plurkEasyEmoticon.updaterZIndex;
+                        plurkEasyEmoticon.updater.style.zIndex = '10000';
                     }
-                    plurkEasyEmoticon.isBarVisible = false;
-                }
-            } else {
-                if (plurkEasyEmoticon.isEnabled) {
-                    if (y < plurkEasyEmoticon.barHeight) {
-                        plurkEasyEmoticon.easyDiv.style.display = 'block';
-                        if (plurkEasyEmoticon.updater) {
-                            plurkEasyEmoticon.updater.style.zIndex = '10000';
-                        }
-                        plurkEasyEmoticon.isBarVisible = true;
-                    }
+                    plurkEasyEmoticon.isBarVisible = true;
                 }
             }
         }, true);
 
+        // Using barHeight to hide div is wrong, the div height can be greater
+        // than barHeight if window width is small, so we detect the mouseout
+        this.easyDiv.addEventListener('mouseout', function(event) {
+            var e = event.relatedTarget;
+            if (!e || e.parentNode == this || e == this) {
+                return;
+            }
+            if (plurkEasyEmoticon.isBarVisible) {
+                plurkEasyEmoticon.easyDiv.style.display = 'none';
+                if (plurkEasyEmoticon.updater) {
+                    plurkEasyEmoticon.updater.style.zIndex = plurkEasyEmoticon.updaterZIndex;
+                }
+                plurkEasyEmoticon.isBarVisible = false;
+            }
+        }, true);
+        
         window.addEventListener('focus', function(event) {
             var id = event.target.id;
 
